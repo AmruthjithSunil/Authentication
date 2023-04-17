bc5b52004840ed16d08e769e27bc3faf0cf07d4b
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-04-17 23:41:17 +0530

    ecommerce task 20: storing token in localstorage

--- src/store/LoginProvider.js
+++ src/store/LoginProvider.js
@@ -2,13 +2,17 @@ import { useState } from "react";
 import LoginContext from "./login-context";
 
 export default function LoginProvider({ children }) {
-  const [token, setToken] = useState();
-  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const [isLoggedin, setIsLoggedin] = useState(
+    localStorage.getItem("token") != ""
+  );
 
   const loginContext = {
     isLoggedin,
     token,
     updateToken(token) {
+      localStorage.setItem("token", token);
       setToken(token);
     },
     updateIsLoggedin(status) {
